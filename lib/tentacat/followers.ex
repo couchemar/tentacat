780853aa6de0782ac5f5259b69b9cfd578a73c99
Iuri Fernandes <iurifq@gmail.com>
2013-06-24 20:59:02 -0300

    Add functions to get users following and followers of a given user

--- lib/tentacat/followers.ex
+++ lib/tentacat/followers.ex
@@ -2,30 +2,40 @@ defmodule Tentacat.Users.Followers do
   import Tentacat.Client
 
   @doc """
-  List followers for the authenticated user
+  List users that the authenticated user or a given user follow
 
   ## Example
 
+      Tentacat.Users.Followers.following("edgurgel")
       Tentacat.Users.Followers.following(access_token: "91898192219")
 
   More info at: http://developer.github.com/v3/users/followers/#list-users-followed-by-another-user
   """
-  @spec following(Client.auth) :: :jsx.json_term
-  def following(auth) do
+  @spec following(binary | Client.auth) :: :jsx.json_term
+  def following(auth) when is_list(auth) do
     get "user/following", auth
   end
 
+  def following(user_name) when is_binary(user_name) do
+    get "users/#{user_name}/following"
+  end
+
   @doc """
-  List users the authenticated user follows
+  List users that follow the authenticated user or a given user
 
   ## Example
 
+      Tentacat.Users.Followers.followers("edgurgel")
       Tentacat.Users.Followers.followers(access_token: "91898192219")
 
   More info at: http://developer.github.com/v3/users/followers/#list-followers-of-a-user
   """
-  @spec followers(Client.auth) :: :jsx.json_term
-  def followers(auth) do
+  @spec followers(binary | Client.auth) :: :jsx.json_term
+  def followers(auth) when is_list(auth) do
     get "user/followers", auth
+  end
+
+  def followers(user_name) when is_binary(user_name) do
+    get "users/#{user_name}/followers"
   end
 end
